8e5f3d295bee481ce0167da6bb629da794604d4f
stalin vela <stalinmsc@hotmail.es>
2020-06-08 04:05:01 -0500

    Interfaz de usuario agregada

---
+++ ProyecCertificacion/UIEjercicio/frmAlumno.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace UIEjercicio
+{
+    partial class frmAlumno
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtapellido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtcedula = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtlugar = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.stpfecha = new System.Windows.Forms.DateTimePicker();
+            this.rbmasculino = new System.Windows.Forms.RadioButton();
+            this.rbfemenino = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lsAlumnos = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnguardar = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lsAlumnos)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombres:";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(218, 79);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(254, 22);
+            this.txtnombre.TabIndex = 1;
+            // 
+            // txtapellido
+            // 
+            this.txtapellido.Location = new System.Drawing.Point(218, 128);
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(254, 22);
+            this.txtapellido.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Apellidos:";
+            // 
+            // txtcedula
+            // 
+            this.txtcedula.Location = new System.Drawing.Point(218, 179);
+            this.txtcedula.Name = "txtcedula";
+            this.txtcedula.Size = new System.Drawing.Size(254, 22);
+            this.txtcedula.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(152, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cedula:";
+            // 
+            // txtlugar
+            // 
+            this.txtlugar.Location = new System.Drawing.Point(218, 231);
+            this.txtlugar.Name = "txtlugar";
+            this.txtlugar.Size = new System.Drawing.Size(254, 22);
+            this.txtlugar.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Lugar de Nacimiento:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(67, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Fecha de Nacimiento:";
+            // 
+            // stpfecha
+            // 
+            this.stpfecha.Location = new System.Drawing.Point(218, 277);
+            this.stpfecha.Name = "stpfecha";
+            this.stpfecha.Size = new System.Drawing.Size(254, 22);
+            this.stpfecha.TabIndex = 9;
+            // 
+            // rbmasculino
+            // 
+            this.rbmasculino.AutoSize = true;
+            this.rbmasculino.Checked = true;
+            this.rbmasculino.Location = new System.Drawing.Point(32, 21);
+            this.rbmasculino.Name = "rbmasculino";
+            this.rbmasculino.Size = new System.Drawing.Size(92, 21);
+            this.rbmasculino.TabIndex = 10;
+            this.rbmasculino.TabStop = true;
+            this.rbmasculino.Text = "Masculino";
+            this.rbmasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbfemenino
+            // 
+            this.rbfemenino.AutoSize = true;
+            this.rbfemenino.Location = new System.Drawing.Point(32, 62);
+            this.rbfemenino.Name = "rbfemenino";
+            this.rbfemenino.Size = new System.Drawing.Size(91, 21);
+            this.rbfemenino.TabIndex = 11;
+            this.rbfemenino.Text = "Femenino";
+            this.rbfemenino.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbmasculino);
+            this.groupBox1.Controls.Add(this.rbfemenino);
+            this.groupBox1.Location = new System.Drawing.Point(218, 332);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 113);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(330, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(305, 32);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Registro de Alumnos ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.toolStrip1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtnombre);
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.stpfecha);
+            this.groupBox2.Controls.Add(this.txtapellido);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtlugar);
+            this.groupBox2.Controls.Add(this.txtcedula);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 63);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(518, 463);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Formulario";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lsAlumnos);
+            this.groupBox3.Location = new System.Drawing.Point(555, 78);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(444, 451);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Lista";
+            // 
+            // lsAlumnos
+            // 
+            this.lsAlumnos.AllowUserToAddRows = false;
+            this.lsAlumnos.AllowUserToDeleteRows = false;
+            this.lsAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lsAlumnos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsAlumnos.Location = new System.Drawing.Point(3, 18);
+            this.lsAlumnos.Name = "lsAlumnos";
+            this.lsAlumnos.ReadOnly = true;
+            this.lsAlumnos.RowHeadersWidth = 51;
+            this.lsAlumnos.RowTemplate.Height = 24;
+            this.lsAlumnos.Size = new System.Drawing.Size(438, 430);
+            this.lsAlumnos.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnguardar});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 18);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(512, 27);
+            this.toolStrip1.TabIndex = 13;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnguardar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(66, 24);
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // frmAlumno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 550);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label6);
+            this.Name = "frmAlumno";
+            this.Text = "GRUD Alumno";
+            this.Load += new System.EventHandler(this.frmAlumno_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.lsAlumnos)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtapellido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtcedula;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtlugar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker stpfecha;
+        private System.Windows.Forms.RadioButton rbmasculino;
+        private System.Windows.Forms.RadioButton rbfemenino;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView lsAlumnos;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnguardar;
+    }
+}
+
---
+++ ProyecCertificacion/UIEjercicio/frmAlumno.cs
@@ -0,0 +1,56 @@
+﻿using BEUEjercicio;
+using BEUEjercicio.Transactions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIEjercicio
+{
+    public partial class frmAlumno : Form
+    {
+        public frmAlumno()
+        {
+            InitializeComponent();
+        }
+
+
+        private void cargarListado()
+        {
+            lsAlumnos.Rows.Clear();
+            List<Alumno> alumnos = AlumnoBLL.List();
+            lsAlumnos.DataSource = alumnos;
+        }
+        private void frmAlumno_Load(object sender, EventArgs e)
+        {
+            cargarListado();
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Alumno a = new Alumno();
+                a.nombres = txtnombre.Text.Trim();
+                a.apellidos = txtapellido.Text.Trim();
+                a.cedula = txtcedula.Text.Trim();
+                a.lugar_nacimiento = txtlugar.Text.Trim();
+                a.sexo = rbmasculino.Checked ? "M" : "F";
+                a.fecha_nacimiento = stpfecha.Value;
+                AlumnoBLL.Create(a);
+                cargarListado();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+           
+        }
+    }
+}
